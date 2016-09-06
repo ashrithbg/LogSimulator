6f58756e7c86857b34aafd20ab0ee2a5bd93d2ca
Ashrith Bangi <ashrithbg@gmail.com>
2016-09-06 05:00:05 -0400

    removed syntax errors in the shell script

--- generate.sh
+++ generate.sh
@@ -1,23 +1,23 @@
 #!/bin/bash 
 JAVA_HOME=/Library/Java/JavaVirtualMachines/jdk1.8.0_101.jdk/Contents/Home
+
 if [ "$#" -lt 1 ]
 then
 	echo "Please enter a data path"
     exit 3
 fi
 
-if ["$1" = "--help"]
+if [ "$1" == "--help" ]
 then 
 	echo "To run this file: ./generate.sh <DATA_PATH>"
 	exit 3
-end
+fi
 
 if [ "$#" -gt 1 ]
 then
 	echo "Wrong number of arguments : "
     exit 3
 fi
-
 CLASSPATH=/Users/ashrith/Documents/Sites/LogSimulator
 $JAVA_HOME/bin/java -jar $CLASSPATH/logsimulator.jar "$1"
 exit 0
