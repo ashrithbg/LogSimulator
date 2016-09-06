3ff970c3e0382ae8a5d3ee5c00620a9bc4bdca14
Ashrith Bangi <ashrithbg@gmail.com>
2016-09-06 04:27:25 -0400

    changed the shell script to accomodate help option

--- generate.sh
+++ generate.sh
@@ -5,6 +5,19 @@ then
 	echo "Please enter a data path"
     exit 3
 fi
+
+if ["$1" = "--help"]
+then 
+	echo "To run this file: ./generate.sh <DATA_PATH>"
+	exit 3
+end
+
+if [ "$#" -gt 1 ]
+then
+	echo "Wrong number of arguments : "
+    exit 3
+fi
+
 CLASSPATH=/Users/ashrith/Documents/Sites/LogSimulator
 $JAVA_HOME/bin/java -jar $CLASSPATH/logsimulator.jar "$1"
 exit 0
